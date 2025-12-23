4bc468675e6742ce615ddc7205dd4a02f82713c6
Køt <238366106+katkotkatze@users.noreply.github.com>
2025-12-23 19:15:12 -0300

    Add method to list users with balance above a threshold

--- src/main/java/me/dio/service/UserService.java
+++ src/main/java/me/dio/service/UserService.java
@@ -1,10 +1,14 @@
 package me.dio.service;
 
 import me.dio.domain.model.User;
+import java.util.List;
 
 public interface UserService {
 
     User findById(Long id);
 
     User create(User userToCreate);
+
+    // Novo método para listar usuários por saldo
+    List<User> listarPorSaldoMaiorQue(Double saldo);
 }
